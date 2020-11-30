e63c6a88699e05535543738a1d93ba2cab306d2a
Matthias Klan <matthias.klan@gmail.com>
2020-11-30 10:37:15 +0100

    Updates

--- demo/index.js
+++ demo/index.js
@@ -86,15 +86,3 @@ function remountAnylineJS() {
   Anyline.dispose();
   mountAnylineJS(selectedPreset);
 }
-
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/anyline-js-pwa/serviceWorker.js")
-      .then(res => { 
-        console.log("service worker registered"); 
-      })
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
