851d8cd46959162877185491ecfc34de4b6bfa76
Matthias Klan <matthias.klan@gmail.com>
2020-11-30 10:17:09 +0100

    Updates

--- demo/index.js
+++ demo/index.js
@@ -91,7 +91,7 @@ function remountAnylineJS() {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("serviceWorker.js")
       .then(res => { 
         console.log("service worker registered"); 
       })
